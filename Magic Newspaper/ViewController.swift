2de8fc8c49e65b9a2bd9ac3305a340a1a5ddc1a8
Mattia Marini <m.mattia1989@gmail.com>
2019-08-16 16:38:32 +0200

    first push with video rendered

--- Magic Newspaper/ViewController.swift
+++ Magic Newspaper/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import SceneKit
+import SpriteKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
@@ -20,21 +21,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        let configuration = ARImageTrackingConfiguration()
+        
+        if let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "dailyProphet", bundle: Bundle.main) {
+            configuration.trackingImages = trackedImages
+            configuration.maximumNumberOfTrackedImages = 2
+            
+            print("Image found!")
+        }
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -47,16 +47,41 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
+    // MARK: - ARImageTracker Methods
+    
+    
+    
     // MARK: - ARSCNViewDelegate
     
-/*
+
     // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
+        
+        if let imageAnchor = anchor as? ARImageAnchor {
+            let videoNode = SKVideoNode(fileNamed: "\(imageAnchor.referenceImage.name!).mp4")
+            let videoScene = SKScene(size: CGSize(width: 480, height: 360))
+            videoNode.position = CGPoint(
+                x: videoScene.size.width / 2,
+                y: videoScene.size.height / 2
+            )
+            videoNode.yScale = -1.0
+            videoScene.addChild(videoNode)
+            videoNode.play()
+            let plane = SCNPlane(
+                width: imageAnchor.referenceImage.physicalSize.width,
+                height: imageAnchor.referenceImage.physicalSize.height
+            )
+            plane.firstMaterial?.diffuse.contents = videoScene //UIColor(white: 1, alpha: 0.5)
+            let geoPlane = SCNNode(geometry: plane)
+            geoPlane.eulerAngles.x = -.pi/2
+            node.addChildNode(geoPlane)
+            
+        }
      
         return node
     }
-*/
+
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
